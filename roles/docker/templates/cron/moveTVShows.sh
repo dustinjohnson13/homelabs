124ca713e78d3a8c72dd3f1056095c3d992fe521
Dustin Johnson <djohnson@iesmach.com>
2021-11-14 10:30:03 -0600

    Cron job to move completed downloads to tv directory

---
+++ roles/docker/templates/cron/moveTVShows.sh
@@ -0,0 +1,12 @@
+COMPLETED_DIR="{{ completed_downloads_dir }}"
+TV_DIR="{{ tv_dir }}"
+DIR="/tmp/moveTVShows-`date +%s}`"
+
+mkdir -p "$DIR"
+cd "$DIR"
+
+git clone https://github.com/dustinjohnson13/moveTVShows.git
+cd moveTVShows
+
+chmod -R 777 "$COMPLETED_DIR"
+./gradlew run -PtvShowDirectory="$TV_DIR" -PcompletedDownloadsDirectory="$COMPLETED_DIR"
