ff029f57d80b23da9d1a84f813edd48a49a0195b
Dustin Johnson <djohnson@iesmach.com>
2021-11-14 18:13:29 -0600

    Redirect git clone standard error to standard out since it logs to stderr for some reason

--- roles/docker/templates/cron/moveTVShows.sh
+++ roles/docker/templates/cron/moveTVShows.sh
@@ -5,7 +5,7 @@ DIR="/tmp/moveTVShows-`date +%s}`"
 mkdir -p "$DIR"
 cd "$DIR"
 
-git clone https://github.com/dustinjohnson13/moveTVShows.git
+git clone https://github.com/dustinjohnson13/moveTVShows.git 2>&1
 cd moveTVShows
 
 chmod -R 777 "$COMPLETED_DIR"
--- roles/docker/templates/cron/processPhotosAndVideos.sh
+++ roles/docker/templates/cron/processPhotosAndVideos.sh
@@ -3,7 +3,7 @@ DIR="/tmp/processPhotosAndVideos-`date +%s}`"
 mkdir -p "$DIR"
 cd "$DIR"
 
-git clone https://github.com/dustinjohnson13/photo_and_video_processing.git
+git clone https://github.com/dustinjohnson13/photo_and_video_processing.git 2>&1
 cd photo_and_video_processing
 
 . ./functions.sh && verifyRequiredSoftwareExists
