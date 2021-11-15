4a298d986538bf7e1fd1e758849f58e6f5ce9114
Dustin Johnson <djohnson@iesmach.com>
2021-11-15 08:38:37 -0600

    Fully qualify yt-dlp and tmpreaper paths

--- roles/docker/templates/cron/sharingVideoDownloads.sh
+++ roles/docker/templates/cron/sharingVideoDownloads.sh
@@ -1,6 +1,6 @@
 YOUTUBE='https://www.youtube.com'
 BASE_DIR='/data/archives/video/sharing'
-DOWNLOAD='yt-dlp --extract-audio --audio-format mp3 --ignore-errors --download-archive downloaded.txt'
+DOWNLOAD='/usr/local/bin/yt-dlp --extract-audio --audio-format mp3 --ignore-errors --download-archive downloaded.txt'
 
 cd "$BASE_DIR/jocko_podcast" && $DOWNLOAD "$YOUTUBE/channel/UCkqcY4CAuBFNFho6JgygCnA/videos"
 cd "$BASE_DIR/JordanPetersonVideos" && $DOWNLOAD "$YOUTUBE/channel/UCL_f53ZEJxp8TtlOkHwMV9Q"
