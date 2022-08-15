ac50e5ad87b2beb2a1d4e62b07f8f2d36f1743f2
Dustin Johnson <dustinjohnson13+github@gmail.com>
2022-08-15 08:10:30 -0500

    Remove JRE podcast

--- roles/docker/templates/cron/sharingVideoDownloads.sh
+++ roles/docker/templates/cron/sharingVideoDownloads.sh
@@ -4,5 +4,4 @@ DOWNLOAD='/usr/local/bin/yt-dlp --extract-audio --audio-format mp3 --ignore-erro
 
 cd "$BASE_DIR/jocko_podcast" && $DOWNLOAD "$YOUTUBE/channel/UCkqcY4CAuBFNFho6JgygCnA/videos"
 cd "$BASE_DIR/JordanPetersonVideos" && $DOWNLOAD "$YOUTUBE/channel/UCL_f53ZEJxp8TtlOkHwMV9Q/videos"
-cd "$BASE_DIR/powerfuljre" && $DOWNLOAD "$YOUTUBE/user/PowerfulJRE/videos"
 cd "$BASE_DIR/sterling_and_stone" && $DOWNLOAD "$YOUTUBE/user/SterlingandStone/videos"
